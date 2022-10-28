e1a6223864b30b966a7a7514d2e369cf28c8249c
Martin Blicha <martin.blicha@usi.ch>
2022-10-28 15:24:13 +0200

    Options: Add KIND engine to the helper message

--- src/Options.cc
+++ src/Options.cc
@@ -33,6 +33,7 @@ void printUsage() {
         "-l,--logic <name>          SMT-LIB logic to use (required); possible values: QF_LRA, QF_LIA\n"
         "-e,--engine <name>         Select engine to use; supported engines:\n"
         "                               bmc - Bounded Model Checking (only transition systems)\n"
+        "                               kind - basic k-induction algorithm (only transition systems)\n"
         "                               lawi - Lazy Abstraction with Interpolants (only linear CHC systems)\n"
         "                               spacer - custom implementation of Spacer (any CHC system)\n"
         "                               split-tpa - Split Transition Power Abstraction (only transition systems)\n"
