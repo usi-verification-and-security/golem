3782933a6ee360a4fdba56cb3c1ba9b80c4fc2ba
BritikovKI <BritikovKI@yandex.ru>
2022-12-05 15:27:06 +0100

    working Uroborost
    removed git ci
    
    Comments responded
    
    int to uint

--- src/engine/Uroboros.cc
+++ src/engine/Uroboros.cc
@@ -26,18 +26,16 @@ GraphVerificationResult Uroboros::solveTransitionSystem(TransitionSystem const &
     const char * msg = "ok";
     config.setOption(SMTConfig::o_produce_inter, SMTOption(true), msg);
     config.setSimplifyInterpolant(4);
-    MainSolver solver(logic, config, "Uroboros");
-    Logic & initLogic = logic;
-//    std::cout << "Adding initial states: " << logic.pp(init) << std::endl;
-//    solver.insertFormula(init);
-    { // Check for system with empty initial states
-        auto res = solver.check();
-        if (res == s_False) {
-            return GraphVerificationResult{VerificationResult::SAFE};
-        }
-    }
     TimeMachine tm{logic};
-    for (int k = 1; k < maxLoopUnrollings; ++k) {
+    MainSolver initSolver(logic, config, "Uroboros");
+    initSolver.insertFormula(init);
+    PTRef versionedQuery = tm.sendFlaThroughTime(query, 0);
+    initSolver.insertFormula(versionedQuery);
+    //if I /\ F is Satisfiable, return true
+    if(initSolver.check() == s_True){
+        GraphVerificationResult{VerificationResult::UNSAFE,  InvalidityWitness::fromTransitionSystem(graph, 0)};
+    }
+    for (uint32_t k = 1; k < maxLoopUnrollings; ++k) {
         InterpolantResult res = finiteRun(init, transition, query, k);
         if(res.result == l_True){
             return GraphVerificationResult{VerificationResult::UNSAFE,  InvalidityWitness::fromTransitionSystem(graph, res.depth)};
@@ -49,41 +47,26 @@ GraphVerificationResult Uroboros::solveTransitionSystem(TransitionSystem const &
 }
 
 //procedure FiniteRun(M=(I,T,F), k>0)
-Uroboros::InterpolantResult Uroboros::finiteRun(PTRef & init, PTRef & transition, PTRef & query, int & k) {
+Uroboros::InterpolantResult Uroboros::finiteRun(PTRef init, PTRef transition, PTRef query, int k) {
     SMTConfig config;
     const char * msg = "ok";
     config.setOption(SMTConfig::o_produce_inter, SMTOption(true), msg);
     config.setSimplifyInterpolant(4);
     TimeMachine tm{logic};
     ipartitions_t mask = 0;
-
-    MainSolver initSolver(logic, config, "Uroboros");
-    initSolver.insertFormula(init);
-    PTRef versionedQuery = tm.sendFlaThroughTime(query, 0);
-    initSolver.insertFormula(versionedQuery);
-    //if I /\ F is Satisfiable, return true
-    if(initSolver.check() == s_True){
-        return InterpolantResult{l_True, 0, init};
-    }
     PTRef movingInit = init;
-    printf("init: %s\n", logic.pp(init).c_str());
     int iter = 0;
     // while true
     while(true){
         MainSolver solver(logic, config, "Uroboros");
         //A = CNF(PREF1(M'), U1)
-        solver.insertFormula(init);
         PTRef A = logic.mkAnd(movingInit, tm.sendFlaThroughTime(transition, iter));
-        printf("A: %s\n", logic.pp(A).c_str());
         solver.insertFormula(A);
         //B = CNF(SUFFk(M'), U2)
-        solver.push();
         PTRef B = tm.sendFlaThroughTime(query, iter + k);
-//        logic.pp();
         for(int i = iter + 1; i < iter + k; i++){
             B = logic.mkAnd(B, tm.sendFlaThroughTime(transition, i));
         }
-        printf("B: %s\n", logic.pp(B).c_str());
         solver.insertFormula(B);
         // Run SAT on A U B.
         auto res = solver.check();
@@ -101,18 +84,15 @@ Uroboros::InterpolantResult Uroboros::finiteRun(PTRef & init, PTRef & transition
             opensmt::setbit(mask, 0);
             //opensmt::setbit(mask, 1);
             //let P = Itp(P, A, B)
-            //let R' = P(W/W0)
+            //let R' = P<W/W0>
             PTRef itp = lastIterationInterpolant(solver, mask);
-            printf("Itp: %s\n", logic.pp(itp).c_str());
-            movingInit = tm.sendFlaThroughTime(movingInit, iter+1);
-            printf("New init: %s\n", logic.pp(init).c_str());
+            movingInit = tm.sendFlaThroughTime(movingInit, 1);
             //if R' => R return False(if R' /\ not R returns True)
-            if(checkItp(itp, movingInit) == s_True){
-                return InterpolantResult{l_False, iter+k, itp};
+            if(checkItp(itp, movingInit) == s_False){
+                return InterpolantResult{l_False, iter+k, tm.sendFlaThroughTime(movingInit, -iter-1)};
             }
             // let R = R\/R'
             movingInit = logic.mkOr(movingInit, itp);
-            printf("Moving init: %s\n", logic.pp(init).c_str());
         }
         iter++;
     }
