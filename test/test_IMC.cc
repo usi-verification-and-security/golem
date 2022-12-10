9a2c1afcfb3b4fc3f07970639f843ebf92fa30af
Martin Blicha <martin.blicha@usi.ch>
2022-12-10 15:52:17 +0100

    Tests: Simplify IMC tests

--- test/test_IMC.cc
+++ test/test_IMC.cc
@@ -1,134 +1,70 @@
 /*
- * Copyright (c) 2021-2022, Martin Blicha <martin.blicha@gmail.com>
+ * Copyright (c) 2022, Konstantin Britikov <britikovki@gmail.com>
+ * Copyright (c) 2022, Martin Blicha <martin.blicha@gmail.com>
  *
  * SPDX-License-Identifier: MIT
  */
 
-#include <gtest/gtest.h>
+#include "TestTemplate.h"
 #include "engine/IMC.h"
-#include "Validator.h"
 
 
 
-class IMCTest : public ::testing::Test {
-protected:
-    ArithLogic logic{opensmt::Logic_t::QF_LIA};
-    Options options;
-    ChcSystem system;
-    PTRef zero;
-    PTRef one;
-    PTRef two;
-    PTRef x, xp;
-
-    IMCTest() {
-        zero = logic.getTerm_IntZero();
-        one = logic.getTerm_IntOne();
-        two = logic.mkIntConst(2);
-        x = mkIntVar("x");
-        xp = mkIntVar("xp");
-    }
-
-    PTRef mkIntVar(char const * const name) { return logic.mkIntVar(name); }
-
-    PTRef mkBoolVar(char const * const name) { return logic.mkBoolVar(name); }
-
-    SRef boolSort() const { return logic.getSort_bool(); }
-
-    SRef intSort() const { return logic.getSort_int(); }
-
-    SymRef mkPredicateSymbol(std::string const & name, vec<SRef> const & argSorts) {
-        SymRef sym = logic.declareFun(name, boolSort(), argSorts);
-        system.addUninterpretedPredicate(sym);
-        return sym;
-    }
-
-    PTRef instantiatePredicate(SymRef symbol, vec<PTRef> const & args) { return logic.mkUninterpFun(symbol, args); }
-
-    void solveSystem(std::vector<ChClause> const & clauses, Engine & engine, VerificationAnswer expectedResult, bool validate) {
-        for (auto const & clause : clauses) { system.addClause(clause); }
-
-        auto normalizedSystem = Normalizer(logic).normalize(system);
-        auto hypergraph = ChcGraphBuilder(logic).buildGraph(normalizedSystem);
-        auto res = engine.solve(*hypergraph);
-        auto answer = res.getAnswer();
-        ASSERT_EQ(answer, expectedResult);
-        if (validate) {
-            auto validationResult = Validator(logic).validate(*hypergraph, res);
-            ASSERT_EQ(validationResult, Validator::Result::VALIDATED);
-        }
-    }
-};
+class IMCTest : public LIAEngineTest {};
 
 
 TEST_F(IMCTest, test_IMC_simple) {
-    ArithLogic logic {opensmt::Logic_t::QF_LIA};
     Options options;
-    options.addOption(Options::LOGIC, "QF_LIA");
-    options.addOption(Options::COMPUTE_WITNESS, "true");
-    SymRef s1 = logic.declareFun("s1", logic.getSort_bool(), {logic.getSort_int()});
-    PTRef x = logic.mkIntVar("x");
-    PTRef xp = logic.mkIntVar("xp");
-    PTRef current = logic.mkUninterpFun(s1, {x});
-    PTRef next = logic.mkUninterpFun(s1, {xp});
-    ChcSystem system;
-    system.addUninterpretedPredicate(s1);
-    system.addClause( // x' = 0 => s1(x')
-            ChcHead{UninterpretedPredicate{next}},
-            ChcBody{{logic.mkEq(xp, logic.getTerm_IntZero())}, {}});
-    system.addClause( // s1(x) and x' = x + 1 => s1(x')
-            ChcHead{UninterpretedPredicate{next}},
-            ChcBody{{logic.mkEq(xp, logic.mkPlus(x, logic.getTerm_IntOne()))}, {UninterpretedPredicate{current}}}
-    );
-    system.addClause( // s1(x) and x > 1 => false
-            ChcHead{UninterpretedPredicate{logic.getTerm_false()}},
-            ChcBody{{logic.mkGt(x, logic.getTerm_IntOne())}, {UninterpretedPredicate{current}}}
-    );
-    auto normalizedSystem = Normalizer(logic).normalize(system);
-    auto hypergraph = ChcGraphBuilder(logic).buildGraph(normalizedSystem);
-    ASSERT_TRUE(hypergraph->isNormalGraph());
-    auto graph = hypergraph->toNormalGraph();
-    IMC imc(logic, options);
-    auto res = imc.solve(*graph);
-    auto answer = res.getAnswer();
-    ASSERT_EQ(answer, VerificationAnswer::UNSAFE);
-    auto witness = res.getInvalidityWitness();
-    auto validationResult = Validator(logic).validate(*hypergraph, res);
-    ASSERT_EQ(validationResult, Validator::Result::VALIDATED);
-}
-
-
-TEST_F(IMCTest, test_IMC_simple_safe)
-{
-    Options options;
-    options.addOption(Options::LOGIC, "QF_LIA");
     options.addOption(Options::COMPUTE_WITNESS, "true");
     SymRef s1 = mkPredicateSymbol("s1", {intSort()});
     PTRef current = instantiatePredicate(s1, {x});
     PTRef next = instantiatePredicate(s1, {xp});
-    // x = 0 => S1(x)
-    // S1(x) and x' = x + 1 => S1(x')
-    // S1(x) and x < 0 => false
+    std::vector<ChClause> clauses {
+        { // x' = 0 => s1(x')
+            ChcHead{UninterpretedPredicate{next}},
+            ChcBody{{logic->mkEq(xp, zero)}, {}}
+        },
+        { // s1(x) and x' = x + 1 => s1(x')
+            ChcHead{UninterpretedPredicate{next}},
+            ChcBody{{logic->mkEq(xp, logic->mkPlus(x, one))}, {UninterpretedPredicate{current}}}
+        },
+        { // s1(x) and x > 1 => false
+            ChcHead{UninterpretedPredicate{logic->getTerm_false()}},
+            ChcBody{{logic->mkGt(x, one)}, {UninterpretedPredicate{current}}}
+        }
+    };
+    IMC imc(*logic, options);
+    solveSystem(clauses, imc, VerificationAnswer::UNSAFE, true);
+}
+
+
+TEST_F(IMCTest, test_IMC_simple_safe) {
+    Options options;
+    options.addOption(Options::COMPUTE_WITNESS, "true");
+    SymRef s1 = mkPredicateSymbol("s1", {intSort()});
+    PTRef current = instantiatePredicate(s1, {x});
+    PTRef next = instantiatePredicate(s1, {xp});
+
     std::vector<ChClause> clauses{
-            {
-                    ChcHead{UninterpretedPredicate{next}},
-                    ChcBody{{logic.mkEq(xp, zero)}, {}}
-            },
-            {
-                    ChcHead{UninterpretedPredicate{next}},
-                    ChcBody{{logic.mkEq(xp, logic.mkPlus(x, one))}, {UninterpretedPredicate{current}}}
-            },
-            {
-                    ChcHead{UninterpretedPredicate{logic.getTerm_false()}},
-                    ChcBody{{logic.mkLt(x, zero)}, {UninterpretedPredicate{current}}}
-            }};
-    IMC engine(logic, options);
+        { // x = 0 => S1(x)
+            ChcHead{UninterpretedPredicate{next}},
+            ChcBody{{logic->mkEq(xp, zero)}, {}}
+        },
+        { // S1(x) and x' = x + 1 => S1(x')
+            ChcHead{UninterpretedPredicate{next}},
+            ChcBody{{logic->mkEq(xp, logic->mkPlus(x, one))}, {UninterpretedPredicate{current}}}
+        },
+        { // S1(x) and x < 0 => false
+            ChcHead{UninterpretedPredicate{logic->getTerm_false()}},
+            ChcBody{{logic->mkLt(x, zero)}, {UninterpretedPredicate{current}}}
+        }
+    };
+    IMC engine(*logic, options);
     solveSystem(clauses, engine, VerificationAnswer::SAFE, true);
 }
 
-TEST_F(IMCTest, test_IMC_moreInductionForward_safe)
-{
+TEST_F(IMCTest, test_IMC_moreInductionForward_safe) {
     Options options;
-    options.addOption(Options::LOGIC, "QF_LIA");
     options.addOption(Options::COMPUTE_WITNESS, "true");
     SymRef s1 = mkPredicateSymbol("s1", {intSort()});
     PTRef current = instantiatePredicate(s1, {x});
@@ -137,26 +73,25 @@ TEST_F(IMCTest, test_IMC_moreInductionForward_safe)
     // S1(x) and x' = ite(x = 10, 0, x + 1) => S1(x')
     // S1(x) and x = 15 => false
     std::vector<ChClause> clauses{
-            {
-                    ChcHead{UninterpretedPredicate{next}},
-                    ChcBody{{logic.mkEq(xp, zero)}, {}}
-            },
-            {
-                    ChcHead{UninterpretedPredicate{next}},
-                    ChcBody{{logic.mkEq(xp, logic.mkIte(logic.mkEq(x, logic.mkIntConst(10)), zero, logic.mkPlus(x, one)))}, {UninterpretedPredicate{current}}}
-            },
-            {
-                    ChcHead{UninterpretedPredicate{logic.getTerm_false()}},
-                    ChcBody{{logic.mkEq(x, logic.mkIntConst(15))}, {UninterpretedPredicate{current}}}
-            }};
-    IMC engine(logic, options);
+        {
+            ChcHead{UninterpretedPredicate{next}},
+            ChcBody{{logic->mkEq(xp, zero)}, {}}
+        },
+        {
+            ChcHead{UninterpretedPredicate{next}},
+            ChcBody{{logic->mkEq(xp, logic->mkIte(logic->mkEq(x, logic->mkIntConst(10)), zero, logic->mkPlus(x, one)))}, {UninterpretedPredicate{current}}}
+        },
+        {
+            ChcHead{UninterpretedPredicate{logic->getTerm_false()}},
+            ChcBody{{logic->mkEq(x, logic->mkIntConst(15))}, {UninterpretedPredicate{current}}}
+        }
+    };
+    IMC engine(*logic, options);
     solveSystem(clauses, engine, VerificationAnswer::SAFE, true);
 }
 
-TEST_F(IMCTest, test_IMC_moreInductionBackward_safe)
-{
+TEST_F(IMCTest, test_IMC_moreInductionBackward_safe) {
     Options options;
-    options.addOption(Options::LOGIC, "QF_LIA");
     options.addOption(Options::COMPUTE_WITNESS, "true");
     SymRef s1 = mkPredicateSymbol("s1", {intSort()});
     PTRef current = instantiatePredicate(s1, {x});
@@ -165,18 +100,19 @@ TEST_F(IMCTest, test_IMC_moreInductionBackward_safe)
     // S1(x) and x' = ite(x = 5, 0, x + 1) => S1(x')
     // S1(x) and x = 15 => false
     std::vector<ChClause> clauses{
-            {
-                    ChcHead{UninterpretedPredicate{next}},
-                    ChcBody{{logic.mkEq(xp, zero)}, {}}
-            },
-            {
-                    ChcHead{UninterpretedPredicate{next}},
-                    ChcBody{{logic.mkEq(xp, logic.mkIte(logic.mkEq(x, logic.mkIntConst(5)), zero, logic.mkPlus(x, one)))}, {UninterpretedPredicate{current}}}
-            },
-            {
-                    ChcHead{UninterpretedPredicate{logic.getTerm_false()}},
-                    ChcBody{{logic.mkEq(x, logic.mkIntConst(15))}, {UninterpretedPredicate{current}}}
-            }};
-    IMC engine(logic, options);
+        {
+            ChcHead{UninterpretedPredicate{next}},
+            ChcBody{{logic->mkEq(xp, zero)}, {}}
+        },
+        {
+            ChcHead{UninterpretedPredicate{next}},
+            ChcBody{{logic->mkEq(xp, logic->mkIte(logic->mkEq(x, logic->mkIntConst(5)), zero, logic->mkPlus(x, one)))}, {UninterpretedPredicate{current}}}
+        },
+        {
+            ChcHead{UninterpretedPredicate{logic->getTerm_false()}},
+            ChcBody{{logic->mkEq(x, logic->mkIntConst(15))}, {UninterpretedPredicate{current}}}
+        }
+    };
+    IMC engine(*logic, options);
     solveSystem(clauses, engine, VerificationAnswer::SAFE, true);
 }
