74d8f100186d4fe4028e0a28725ed4940992c002
Konstantin <BritikovKI@Yandex.ru>
2023-05-06 21:14:11 +0200

    Picky: quick fix of TPA

--- src/engine/TPA.cc
+++ src/engine/TPA.cc
@@ -137,9 +137,9 @@ public:
     SolverWrapperSingleUse(Logic & logic, PTRef transition) : logic(logic) {
         this->transition = transition;
         const char * msg = "ok";
-//        config.setOption(SMTConfig::o_sat_pure_lookahead, SMTOption(true), msg);
-//        config.setOption(SMTConfig::o_sat_picky_w, 10, msg);
-        config.setOption(SMTConfig::o_random_seed, SMTOption(526899046), msg);
+        config.setOption(SMTConfig::o_sat_picky, SMTOption(true), msg);
+        config.setOption(SMTConfig::o_sat_picky_w, 10, msg);
+//        config.setOption(SMTConfig::o_random_seed, SMTOption(526899046), msg);
         config.setOption(SMTConfig::o_produce_models, SMTOption(true), msg);
         config.setOption(SMTConfig::o_produce_inter, SMTOption(true), msg);
         config.setSimplifyInterpolant(4);
@@ -199,10 +199,10 @@ public:
         //        std::cout << "Transition: " << logic.printTerm(transition) << std::endl;
         this->transition = transition;
         const char * msg = "ok";
-        config.setOption(SMTConfig::o_random_seed, SMTOption(526899046), msg);
+//        config.setOption(SMTConfig::o_random_seed, SMTOption(526899046), msg);
 //        config.setOption(SMTConfig::o_sat_pure_lookahead, SMTOption(true), msg);
-//        config.setOption(SMTConfig::o_sat_picky, SMTOption(true), msg);
-//        config.setOption(SMTConfig::o_sat_picky_w, 10, msg);
+        config.setOption(SMTConfig::o_sat_picky, SMTOption(true), msg);
+        config.setOption(SMTConfig::o_sat_picky_w, 10, msg);
         config.setOption(SMTConfig::o_produce_models, SMTOption(true), msg);
         config.setOption(SMTConfig::o_produce_inter, SMTOption(true), msg);
         config.setSimplifyInterpolant(4);
