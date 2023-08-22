77c160d090ec55b768804f0fcf61a47c8e459cca
Matias Barandiaran <matias.barandiaran03@gmail.com>
2023-08-22 16:30:24 +0200

    Build Files Exclusion
    Removal of build files

--- cmake-build-release/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mambo/golem
-# Build directory: /home/mambo/golem/cmake-build-release
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
