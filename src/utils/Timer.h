8d25f1536788c9312424a269910a6d66893b9685
Martin Blicha <martin.blicha@gmail.com>
2024-11-17 15:42:02 +0100

    Utils: Add a simple Timer to measure elapsed time

---
+++ src/utils/Timer.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2024, Martin Blicha <martin.blicha@gmail.com>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef GOLEM_TIMER_H
+#define GOLEM_TIMER_H
+
+#include <chrono>
+
+struct Timer {
+    Timer() : start(std::chrono::high_resolution_clock::now()) {}
+    auto elapsedMilliseconds() {
+        return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start)
+            .count();
+    }
+
+private:
+    std::chrono::high_resolution_clock::time_point start;
+};
+
+#endif // GOLEM_TIMER_H
