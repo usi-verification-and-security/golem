7430b7e35733ad53668c495a8db420f69f7873f5
Martin Blicha <martin.blicha@gmail.com>
2025-02-03 13:26:53 +0100

    QE: Simple refactoring

--- src/QuantifierElimination.cc
+++ src/QuantifierElimination.cc
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2021-2023, Martin Blicha <martin.blicha@gmail.com>
+* Copyright (c) 2021-2025, Martin Blicha <martin.blicha@gmail.com>
 *
 * SPDX-License-Identifier: MIT
 */
@@ -10,40 +10,19 @@
 #include "TermUtils.h"
 #include "utils/SmtSolver.h"
 
-PTRef QuantifierElimination::keepOnly(PTRef fla, const vec<PTRef> & varsToKeep) {
-    auto allVars = TermUtils(logic).getVars(fla);
-    vec<PTRef> toEliminate;
-    for (PTRef var : allVars) {
-        if (std::find(varsToKeep.begin(), varsToKeep.end(), var) == varsToKeep.end()) {
-            toEliminate.push(var);
-        }
-    }
-    return eliminate(fla, toEliminate);
-}
-
-PTRef QuantifierElimination::eliminate(PTRef fla, PTRef var) {
-    return eliminate(fla, vec<PTRef>{var});
-}
-
-PTRef QuantifierElimination::eliminate(PTRef fla, vec<PTRef> const & vars) {
-    if (not std::all_of(vars.begin(), vars.end(), [this](PTRef var){ return logic.isVar(var); }) or not logic.hasSortBool(fla)) {
-        throw std::invalid_argument("Invalid arguments to quantifier elimination");
-    }
-
-    fla = TermUtils(logic).toNNF(fla);
+namespace {
+PTRef eliminate(Logic & logic, PTRef fla, vec<PTRef> const & vars) {
     vec<PTRef> projections;
 
     SMTSolver solver(logic, SMTSolver::WitnessProduction::ONLY_MODEL);
     solver.assertProp(fla);
-    while(true) {
+    while (true) {
         auto res = solver.check();
-        if (res == SMTSolver::Answer::UNSAT) {
-            break;
-        } else if (res == SMTSolver::Answer::SAT) {
+        if (res == SMTSolver::Answer::UNSAT) { break; }
+        if (res == SMTSolver::Answer::SAT) {
             auto model = solver.getModel();
             ModelBasedProjection mbp(logic);
             PTRef projection = mbp.project(fla, vars, *model);
-//            std::cout << "Projection: " << logic.printTerm(projection) << std::endl;
             projections.push(projection);
             solver.push(); // to avoid processing the same formula over and over again
             solver.assertProp(logic.mkNot(projection));
@@ -58,4 +37,28 @@ PTRef QuantifierElimination::eliminate(PTRef fla, vec<PTRef> const & vars) {
         // TODO: more simplifications?
     }
     return result;
+}
+} // namespace
+
+PTRef QuantifierElimination::keepOnly(PTRef fla, const vec<PTRef> & varsToKeep) {
+    auto allVars = TermUtils(logic).getVars(fla);
+    vec<PTRef> toEliminate;
+    for (PTRef var : allVars) {
+        if (std::find(varsToKeep.begin(), varsToKeep.end(), var) == varsToKeep.end()) { toEliminate.push(var); }
+    }
+    return eliminate(fla, toEliminate);
+}
+
+PTRef QuantifierElimination::eliminate(PTRef fla, PTRef var) {
+    return eliminate(fla, vec<PTRef>{var});
+}
+
+PTRef QuantifierElimination::eliminate(PTRef fla, vec<PTRef> const & vars) {
+    if (not std::all_of(vars.begin(), vars.end(), [this](PTRef var) { return logic.isVar(var); }) or
+        not logic.hasSortBool(fla)) {
+        throw std::invalid_argument("Invalid arguments to quantifier elimination");
+    }
+
+    fla = TermUtils(logic).toNNF(fla);
+    return ::eliminate(logic, fla, vars);
 }
--- src/QuantifierElimination.h
+++ src/QuantifierElimination.h
@@ -1,6 +1,8 @@
-//
-// Created by Martin Blicha on 12.06.21.
-//
+/*
+ * Copyright (c) 2021-2025, Martin Blicha <martin.blicha@gmail.com>
+ *
+ * SPDX-License-Identifier: MIT
+ */
 
 #ifndef OPENSMT_QUANTIFIERELIMINATION_H
 #define OPENSMT_QUANTIFIERELIMINATION_H
@@ -13,14 +15,15 @@
  * Given a formula F(x,y) we want to compute a formula G(x) such that G(x) \equiv \exist y F(x,y)
  */
 class QuantifierElimination {
-    Logic & logic;
 public:
-    QuantifierElimination(Logic & logic) : logic(logic) {}
+    explicit QuantifierElimination(Logic & logic) : logic(logic) {}
 
     PTRef eliminate(PTRef fla, PTRef var);
     PTRef eliminate(PTRef fla, vec<PTRef> const & vars);
     PTRef keepOnly(PTRef, vec<PTRef> const & vars);
-};
 
+private:
+    Logic & logic;
+};
 
 #endif //OPENSMT_QUANTIFIERELIMINATION_H
