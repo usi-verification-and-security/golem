c126f6039ca4e88d63e91b1663439c681c018c7c
Martin Blicha <martin.blicha@gmail.com>
2025-04-01 11:05:31 +0200

    Update OpenSMT to 2.9.0 and C++ to C++20

--- src/include/osmt_terms.h
+++ src/include/osmt_terms.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2022, Martin Blicha <martin.blicha@gmail.com>
+ * Copyright (c) 2021-2025, Martin Blicha <martin.blicha@gmail.com>
  *
  * SPDX-License-Identifier: MIT
  */
@@ -8,15 +8,15 @@
 #define GOLEM_OSMT_TERMS_H
 
 #ifdef OPENSMT_LOCAL_BUILD
-#include "ArithLogic.h"
-#include "PTRef.h"
-#include "SymRef.h"
-#include "TreeOps.h"
-#include "BoolRewriting.h"
-#include "Substitutor.h"
-#include "DivModRewriter.h"
-#include "DistinctRewriter.h"
-#include "IteHandler.h"
+#include "logics/ArithLogic.h"
+#include "pterms/PTRef.h"
+#include "symbols/SymRef.h"
+#include "common/TreeOps.h"
+#include "simplifiers//BoolRewriting.h"
+#include "rewriters/Substitutor.h"
+#include "rewriters/DivModRewriter.h"
+#include "rewriters/DistinctRewriter.h"
+#include "itehandler/IteHandler.h"
 #else
 #include "opensmt/ArithLogic.h"
 #include "opensmt/PTRef.h"
@@ -28,5 +28,7 @@
 #include "opensmt/DistinctRewriter.h"
 #include "opensmt/IteHandler.h"
 #endif // OPENSMT_LOCAL_BUILD
+
+using namespace opensmt;
 
 #endif //GOLEM_OSMT_TERMS_H
