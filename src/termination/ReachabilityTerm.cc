aae4b82f245a1fb6a4d8440fc743608600c465cf
BritikovKI <BritikovKI@yandex.ru>
2025-09-18 18:52:54 +0200

    Updates to the golem

--- src/termination/ReachabilityTerm.cc
+++ src/termination/ReachabilityTerm.cc
@@ -28,6 +28,7 @@ namespace golem::termination {
 
 ReachabilityTerm::Answer ReachabilityTerm::nontermination(ChcDirectedHyperGraph const & system) {
     ArithLogic & logic = dynamic_cast<ArithLogic &>(system.getLogic());
+    TermUtils utils {logic};
 
     assert(system.isNormalGraph());
     auto graph = system.toNormalGraph();
@@ -48,14 +49,34 @@ ReachabilityTerm::Answer ReachabilityTerm::nontermination(ChcDirectedHyperGraph 
         return std::move(ts);
     }();
     // auto engine = EngineFactory(logic, options).getEngine(options.getOrDefault(Options::ENGINE, "spacer"));
+    //     engine->solve();
     auto solver = std::make_unique<TPASplit>(logic, options);
     solver->resetTransitionSystem(*ts);
+    PTRef init  = solver->getInit();
+    PTRef transition = solver -> getTransitionRelation();
+    PTRef query = solver -> getQuery();
+    // auto vars = solver -> getStateVars(0);
+    auto next_vars = solver -> getStateVars(1);
+    auto disjuncts = utils.getTopLevelDisjuncts(transition);
+    bool nondet = false;
+    std::unordered_set<PTRef, PTRefHash> nondet_juncts;
+    for (auto & junct : disjuncts) {
+        auto junct_vars = utils.getVars(junct);
+        for (auto & var : next_vars) {
+            if (std::find(junct_vars.begin(), junct_vars.end(), var) == junct_vars.end()) {
+                nondet = true;
+                nondet_juncts.insert(junct);
+                break;
+            }
+        }
+    }
+    PTRef transitionConstraint = logic.getTerm_true();
+    PTRef initConstraint = logic.getTerm_true();
+
     if (std::stoi(options.getOrDefault(Options::VERBOSE, "0")) > 0) { std::cout << "; Searching for nontermination!\n"; }
     auto res = solver->solve();
     while(res == VerificationAnswer::UNSAFE) {
-        PTRef init  = solver->getInit();
         PTRef reached  = solver->getReachedStates();
-        PTRef transition = solver->getTransitionRelation();
         uint num = solver->getTransitionStepCount();
         std::vector formulas {init, TimeMachine(logic).sendFlaThroughTime(reached, num)};
         SMTSolver SMTsolver(logic, SMTSolver::WitnessProduction::ONLY_MODEL);
@@ -67,11 +88,36 @@ ReachabilityTerm::Answer ReachabilityTerm::nontermination(ChcDirectedHyperGraph 
         auto resSMT = SMTsolver.check();
         assert(resSMT == SMTSolver::Answer::SAT);
         auto model = SMTsolver.getModel();
+        bool detected = false;
         for (int j = num; j > 0; j--) {
-            vec<PTRef> toEliminate = solver->getStateVars(j);
-            transitions = ModelBasedProjection(logic).project(transitions, toEliminate, *model);
+            for (auto & disjunct : disjuncts) {
+                SMTSolver smt_solver(logic, SMTSolver::WitnessProduction::NONE);
+                smt_solver.assertProp(model->evaluate(TimeMachine(logic).sendFlaThroughTime(disjunct, j-1)));
+                if (smt_solver.check() == SMTSolver::Answer::SAT) {
+                    if (nondet_juncts.contains(disjunct)) {
+                        transitions = TimeMachine(logic).sendFlaThroughTime(QuantifierElimination(logic).keepOnly(transitions, solver->getStateVars(j)), -j+1);
+                        detected = true;
+                    }
+                    break;
+                }
+            }
+            if (detected) { break; }
+            // vec<PTRef> toEliminate = solver->getStateVars(j);
+            // transitions = ModelBasedProjection(logic).project(transitions, toEliminate, *model);
+
         }
-        solver->resetInitialStates(logic.mkAnd(init, logic.mkNot(transitions)));
+        if (detected) {
+            transitionConstraint = logic.mkAnd(transitionConstraint, logic.mkNot(transitions));
+            solver->resetTransitionSystem(TransitionSystem(logic,
+                std::make_unique<SystemType>(ts->getStateVars(), ts->getAuxiliaryVars(), logic),
+                logic.mkAnd(init, initConstraint),
+                    logic.mkAnd(transition, transitionConstraint),
+                    query));
+        } else {
+            transitions = QuantifierElimination(logic).keepOnly(transitions, solver->getStateVars(0));
+            initConstraint = logic.mkAnd(initConstraint, logic.mkNot(transitions));
+            solver->resetInitialStates(logic.mkAnd(init, initConstraint));
+        }
         res = solver->solve();
     }
     SMTSolver SMTsolver(logic, SMTSolver::WitnessProduction::NONE);
