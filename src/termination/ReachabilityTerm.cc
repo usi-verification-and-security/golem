3d25e55d5703f462724a95c9172b97d806441b7c
BritikovKI <BritikovKI@yandex.ru>
2026-01-23 12:04:29 +0100

    PR cleaning

--- src/termination/ReachabilityTerm.cc
+++ src/termination/ReachabilityTerm.cc
@@ -7,7 +7,6 @@
 #include "ReachabilityTerm.h"
 
 #include "TermUtils.h"
-#include "../ChcSystem.h"
 #include "engine/EngineFactory.h"
 #include "graph/ChcGraphBuilder.h"
 
@@ -17,33 +16,31 @@ ReachabilityTerm::Answer ReachabilityTerm::termination(TransitionSystem const & 
     auto & logic = dynamic_cast<ArithLogic &>(ts.getLogic());
     auto vars = ts.getStateVars();
     uint multiplier = 1;
+
     // Adding a counter variable
     PTRef counter = logic.mkIntVar("counter");
     PTRef counter0 = TimeMachine(logic).getVarVersionZero(counter);
     PTRef counter1 = TimeMachine(logic).sendVarThroughTime(counter0, 1);
     // initial condition: counter > const * (|x_1| + |x_2| + ... + |x_n|)
-    unsigned i = 0;
     PTRef sum = logic.getTerm_IntZero();
     vec<PTRef> sumCheck;
-    for (auto var : vars) {
-        if (logic.isSortInt(logic.getSortRef(var))) {
+    for (auto i = 0; i < vars.size(); i++) {
+        if (logic.isSortInt(logic.getSortRef(vars[i]))) {
             PTRef temp = TimeMachine(logic).getVarVersionZero(logic.mkIntVar(("y" + std::to_string(i)).c_str()));
-            // cond = y_i >=0 /\ (y_i = -1 * x_i \/ y_i = x_i)
-            sumCheck.push(logic.mkAnd(logic.mkOr(logic.mkEq(temp, var), logic.mkEq(temp, logic.mkNeg(var))), logic.mkGeq(temp, logic.getTerm_IntZero())));
+            // y_i >=0 /\ (y_i = -1 * x_i \/ y_i = x_i) <=> y_i = |x_i|
+            sumCheck.push(logic.mkAnd(logic.mkOr(logic.mkEq(temp, vars[i]), logic.mkEq(temp, logic.mkNeg(vars[i]))),
+                                      logic.mkGeq(temp, logic.getTerm_IntZero())));
             // sum = sum + y_i
             sum = logic.mkPlus(sum, temp);
-            i++;
         }
     }
-    // if (sumCheck.size() == 0) {
-    //     sumCheck.push(logic.mkGt(counter0, logic.getTerm_IntZero())); // Needed in case there are no int variables
-    // }
     vars.push_back(counter0);
-    while (true) {
+    while (multiplier < 64) {
         // counter = multiplier * (y_1 + ... + y_n)
-        PTRef countEq =  logic.mkGt(counter0, logic.mkTimes(logic.mkIntConst(Number(multiplier)), sum));
-        // init = init /\ counter = y_1 + ... + y_n /\ (y_1 = |x_1| /\ ... /\ y_n = |x_n|)
-        PTRef init = sumCheck.size() != 0 ? logic.mkAnd({ts.getInit(), countEq, logic.mkAnd(sumCheck)}) : logic.mkAnd({ts.getInit(), logic.mkGt(counter0, logic.getTerm_IntZero())});
+        PTRef countEq = logic.mkEq(counter0, logic.mkTimes(logic.mkIntConst(Number(multiplier)), sum));
+        // init = init /\ counter = multiplier * (y_1 + ... + y_n) /\ (y_1 = |x_1| /\ ... /\ y_n = |x_n|)
+        PTRef init = sumCheck.size() != 0 ? logic.mkAnd({ts.getInit(), countEq, logic.mkAnd(sumCheck)})
+                                          : logic.mkAnd({ts.getInit(), logic.mkGt(counter0, logic.getTerm_IntZero())});
         // transition = transition /\ counter' = counter - 1
         PTRef counterDec = logic.mkEq(counter1, logic.mkMinus(counter0, logic.getTerm_IntOne()));
         PTRef transition = logic.mkAnd(ts.getTransition(), counterDec);
@@ -82,7 +79,7 @@ ReachabilityTerm::Answer ReachabilityTerm::termination(TransitionSystem const & 
                        ChcBody{InterpretedFla{transition}, {UninterpretedPredicate{pred}}});
         chcs.addClause(ChcHead{UninterpretedPredicate{logic.getTerm_false()}},
                        ChcBody{InterpretedFla{query}, {UninterpretedPredicate{pred}}});
-        // ChcPrinter(logic, std::cout).print(chcs);
+
         Normalizer normalizer(logic);
         auto normalizedSystem = normalizer.normalize(chcs);
         auto hypergraph = ChcGraphBuilder(logic).buildGraph(normalizedSystem);
