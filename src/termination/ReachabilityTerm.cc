6d953ea7c25568748053d77f98d35760716511e9
Konstantin Britikov <31989062+BritikovKI@users.noreply.github.com>
2026-01-28 16:17:43 +0100

    Reachability term vars (#160)

--- src/termination/ReachabilityTerm.cc
+++ src/termination/ReachabilityTerm.cc
@@ -15,87 +15,89 @@ namespace golem::termination {
 ReachabilityTerm::Answer ReachabilityTerm::termination(TransitionSystem const & ts) {
     auto & logic = dynamic_cast<ArithLogic &>(ts.getLogic());
     auto vars = ts.getStateVars();
+    uint multiplier = 1;
+    constexpr uint MAX_MULTIPLIER = 64;
 
     // Adding a counter variable
     PTRef counter = logic.mkIntVar("counter");
     PTRef counter0 = TimeMachine(logic).getVarVersionZero(counter);
     PTRef counter1 = TimeMachine(logic).sendVarThroughTime(counter0, 1);
-    // initial condition: counter > max(0, x_1, -x_1, x_2, -x_2, ..., x_n, -x_n)
-    vec<PTRef> geqCheck;
-    geqCheck.push(logic.mkGt(counter0, logic.getTerm_IntZero())); // Needed in case there are no int variables
+    vec<PTRef> sumArgs;
     for (auto var : vars) {
         if (logic.isSortInt(logic.getSortRef(var))) {
-            geqCheck.push(logic.mkGt(counter0, var));
-            geqCheck.push(logic.mkGt(counter0, logic.mkNeg(var)));
+            // abs(x_i) := ite(x_i >= 0, x_i, -x_i)
+            sumArgs.push(logic.mkIte(logic.mkGeq(var, logic.getTerm_IntZero()), var, logic.mkNeg(var)));
         }
     }
+    PTRef sum = sumArgs.size() == 0 ? logic.getTerm_IntOne() : logic.mkPlus(std::move(sumArgs));
     vars.push_back(counter0);
-    PTRef max = logic.mkAnd(geqCheck);
-    // init = init /\ counter > max(0, x_1, -x_1, x_2, -x_2, ..., x_n, -x_n)
-    PTRef init = logic.mkAnd(ts.getInit(), max);
-    // transition = transition /\ counter' = counter -1
-    PTRef counterDec = logic.mkEq(counter1, logic.mkMinus(counter0, logic.getTerm_IntOne()));
-    PTRef transition = logic.mkAnd(ts.getTransition(), counterDec);
-    // query = counter < 0
-    PTRef query = logic.mkLt(counter0, logic.getTerm_IntZero());
+    while (multiplier < MAX_MULTIPLIER) {
+        // init = init /\ counterInit = multiplier * (|x_1| + ... + |x_n|)
+        PTRef counterInit = logic.mkGt(counter0, logic.mkTimes(logic.mkIntConst(Number(multiplier)), sum));
+        PTRef init = logic.mkAnd(ts.getInit(), counterInit);
+        // transition = transition /\ counter' = counter - 1
+        PTRef counterDec = logic.mkEq(counter1, logic.mkMinus(counter0, logic.getTerm_IntOne()));
+        PTRef transition = logic.mkAnd(ts.getTransition(), counterDec);
+        // query = counter < 0
+        PTRef query = logic.mkLt(counter0, logic.getTerm_IntZero());
 
-    ChcSystem chcs;
-    // Adding an uninterpreted predicate P
-    SymRef predicate = [&]() -> SymRef {
-        vec<SRef> argSorts;
-        for (PTRef var : vars) {
-            argSorts.push(logic.getSortRef(var));
+        ChcSystem chcs;
+        // Adding an uninterpreted predicate P
+        SymRef predicate = [&]() -> SymRef {
+            vec<SRef> argSorts;
+            for (PTRef var : vars) {
+                argSorts.push(logic.getSortRef(var));
+            }
+            return logic.declareFun("P", logic.getSort_bool(), std::move(argSorts));
+        }();
+        chcs.addUninterpretedPredicate(predicate);
+        // creating P(x_1, x_2, ..., x_n)
+        auto pred = [&]() -> PTRef {
+            vec<PTRef> args;
+            for (PTRef var : vars) {
+                args.push(var);
+            }
+            return logic.mkUninterpFun(predicate, std::move(args));
+        }();
+        // creating P(x_1', x_2', ..., x_n')
+        auto pred_next = [&]() -> PTRef {
+            vec<PTRef> args;
+            TimeMachine tm(logic);
+            for (PTRef var : vars) {
+                args.push(tm.sendVarThroughTime(var, 1));
+            }
+            return logic.mkUninterpFun(predicate, std::move(args));
+        }();
+        chcs.addClause(ChcHead{UninterpretedPredicate{pred}}, ChcBody{InterpretedFla{init}, {}});
+        chcs.addClause(ChcHead{UninterpretedPredicate{pred_next}},
+                       ChcBody{InterpretedFla{transition}, {UninterpretedPredicate{pred}}});
+        chcs.addClause(ChcHead{UninterpretedPredicate{logic.getTerm_false()}},
+                       ChcBody{InterpretedFla{query}, {UninterpretedPredicate{pred}}});
+
+        Normalizer normalizer(logic);
+        auto normalizedSystem = normalizer.normalize(chcs);
+        auto hypergraph = ChcGraphBuilder(logic).buildGraph(normalizedSystem);
+        assert(hypergraph->isNormalGraph());
+        // Solving constructed CHC system
+        auto engine = EngineFactory(logic, options).getEngine(options.getOrDefault(Options::ENGINE, "spacer"));
+
+        if (std::stoi(options.getOrDefault(Options::VERBOSE, "0")) > 0) {
+            std::cout << "; Searching for nontermination!\n";
         }
-        return logic.declareFun("P", logic.getSort_bool(), std::move(argSorts));
-    }();
-    chcs.addUninterpretedPredicate(predicate);
-
-    // creating P(x_1, x_2, ..., x_n)
-    auto pred = [&]() -> PTRef {
-        vec<PTRef> args;
-        for (PTRef var : vars) {
-            args.push(var);
+        auto res = engine->solve(*hypergraph);
+        multiplier = multiplier * 2;
+        switch (res.getAnswer()) {
+            case VerificationAnswer::SAFE:
+                return Answer::YES;
+            case VerificationAnswer::UNKNOWN:
+                return Answer::ERROR;
+            case VerificationAnswer::UNSAFE:
+                continue;
         }
-        return logic.mkUninterpFun(predicate, std::move(args));
-    }();
-    // creating P(x_1', x_2', ..., x_n')
-    auto pred_next = [&]() -> PTRef {
-        vec<PTRef> args;
-        TimeMachine tm(logic);
-        for (PTRef var : vars) {
-            args.push(tm.sendVarThroughTime(var, 1));
-        }
-        return logic.mkUninterpFun(predicate, std::move(args));
-    }();
-    chcs.addClause(ChcHead{UninterpretedPredicate{pred}}, ChcBody{InterpretedFla{init}, {}});
-    chcs.addClause(ChcHead{UninterpretedPredicate{pred_next}},
-                   ChcBody{InterpretedFla{transition}, {UninterpretedPredicate{pred}}});
-    chcs.addClause(ChcHead{UninterpretedPredicate{logic.getTerm_false()}},
-                   ChcBody{InterpretedFla{query}, {UninterpretedPredicate{pred}}});
-
-    Normalizer normalizer(logic);
-    auto normalizedSystem = normalizer.normalize(chcs);
-    auto hypergraph = ChcGraphBuilder(logic).buildGraph(normalizedSystem);
-    assert(hypergraph->isNormalGraph());
-
-    // Solving constructed CHC system
-    auto engine = EngineFactory(logic, options).getEngine(options.getOrDefault(Options::ENGINE, "spacer"));
-
-    if (std::stoi(options.getOrDefault(Options::VERBOSE, "0")) > 0) {
-        std::cout << "; Searching for nontermination!\n";
+        assert(false && "Unreachable!");
+        return Answer::ERROR;
     }
-    auto res = engine->solve(*hypergraph);
-
-    switch (res.getAnswer()) {
-        case VerificationAnswer::SAFE:
-            return Answer::YES;
-        case VerificationAnswer::UNKNOWN:
-            return Answer::ERROR;
-        case VerificationAnswer::UNSAFE:
-            return Answer::UNKNOWN;
-    }
-    assert(false && "Unreachable!");
-    return Answer::ERROR;
+    return Answer::UNKNOWN;
 }
 
 } // namespace golem::termination
