5ff54dc45077e515c236e02fc8b02225d1bcded5
Martin Blicha <martin.blicha@gmail.com>
2026-02-18 21:31:55 +0100

    Test: Rename normal forms test and add DNF test

--- test/test_NormalForms.cc
+++ test/test_NormalForms.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2025, Martin Blicha <martin.blicha@gmail.com>
+ * Copyright (c) 2021-2026, Martin Blicha <martin.blicha@gmail.com>
  *
  * SPDX-License-Identifier: MIT
  */
@@ -21,7 +21,7 @@ bool implies(Logic & logic, PTRef antecedent, PTRef consequent) {
 }
 
 
-class NNFTest : public ::testing::Test {
+class NormalFormsTest : public ::testing::Test {
 protected:
     ArithLogic logic {opensmt::Logic_t::QF_LRA};
     PTRef x;
@@ -33,7 +33,7 @@ protected:
     PTRef nc;
     PTRef zero;
     PTRef one;
-    NNFTest() {
+    NormalFormsTest() {
         x = logic.mkRealVar("x");
         a = logic.mkBoolVar("a");
         b = logic.mkBoolVar("b");
@@ -46,13 +46,13 @@ protected:
     }
 };
 
-TEST_F(NNFTest, test_Atom) {
+TEST_F(NormalFormsTest, test_NNF_Atom) {
     PTRef atom = logic.mkLeq(x, zero);
     PTRef nnf = TermUtils(logic).toNNF(atom);
     ASSERT_EQ(atom, nnf);
 }
 
-TEST_F(NNFTest, test_NegatedConjunction) {
+TEST_F(NormalFormsTest, test_NNF_NegatedConjunction) {
     PTRef atom = logic.mkLeq(x, zero);
     PTRef conj = logic.mkAnd(atom, b);
     PTRef fla = logic.mkNot(conj);
@@ -60,7 +60,7 @@ TEST_F(NNFTest, test_NegatedConjunction) {
     ASSERT_EQ(nnf, logic.mkOr(logic.mkNot(atom), logic.mkNot(b)));
 }
 
-TEST_F(NNFTest, test_NegatedDisjunction) {
+TEST_F(NormalFormsTest, test_NNF_NegatedDisjunction) {
     PTRef atom = logic.mkLeq(x, zero);
     PTRef disj = logic.mkOr(atom, b);
     PTRef fla = logic.mkNot(disj);
@@ -68,7 +68,7 @@ TEST_F(NNFTest, test_NegatedDisjunction) {
     ASSERT_EQ(nnf, logic.mkAnd(logic.mkNot(atom), logic.mkNot(b)));
 }
 
-TEST_F(NNFTest, test_BoolEquality) {
+TEST_F(NormalFormsTest, test_NNF_BoolEquality) {
     PTRef eq = logic.mkEq(a,b);
     PTRef nnf = TermUtils(logic).toNNF(eq);
     ASSERT_NE(nnf, eq);
@@ -78,7 +78,7 @@ TEST_F(NNFTest, test_BoolEquality) {
     ));
 }
 
-TEST_F(NNFTest, test_NestedNegatedDisjunction) {
+TEST_F(NormalFormsTest, test_NNF_NestedNegatedDisjunction) {
     //  (= (= a b) c) && (a || ~b)
 
     PTRef eq = logic.mkEq(a, b);
@@ -89,4 +89,12 @@ TEST_F(NNFTest, test_NestedNegatedDisjunction) {
 
     ASSERT_TRUE(implies(logic, fla, nnf));
     ASSERT_TRUE(implies(logic, nnf, fla));
+}
+
+TEST_F(NormalFormsTest, test_DNF_Distribution) {
+    // (a && (b || c))  ==>  ((a && b) || (a && c))
+    PTRef const fla = logic.mkAnd (a, logic.mkOr(b,c));
+    PTRef const dnf = TermUtils(logic).toDNF(fla);
+    PTRef const expected = logic.mkOr(logic.mkAnd(a, b), logic.mkAnd(a, c));
+    EXPECT_EQ(expected, dnf);
 }
