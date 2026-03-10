337eccd734f38963e2caad2edb0b0cb0468eea15
BritikovKI <BritikovKI@yandex.ru>
2026-03-10 11:01:26 +0100

    Minimizing TrInv even more

--- src/termination/ReachabilityNonterm.cc
+++ src/termination/ReachabilityNonterm.cc
@@ -506,7 +506,7 @@ vec<PTRef> extractWellFoundedCandidates(PTRef itp, PTRef sink, ArithLogic & logi
 
     auto sink_disjuncts = utils.getTopLevelDisjuncts(utils.toDNF(unwrapEqs(logic.mkNot(sink), logic)));
     PTRef dnfized_interpolant = utils.simplifyMax(unwrapEqs(itp, logic));
-    std::cout<< "Itp: " <<logic.pp(dnfized_interpolant) << std::endl;
+    // std::cout<< "Itp: " <<logic.pp(dnfized_interpolant) << std::endl;
     dnfized_interpolant = utils.toDNF(dnfized_interpolant);
 
     vec<PTRef> candidates = utils.getTopLevelDisjuncts(dnfized_interpolant);
@@ -767,7 +767,8 @@ ReachabilityNonterm::analyzeTS(PTRef init, PTRef transition, PTRef sink, Options
                 //TODO: Think how to minimize cands in a logical way
                 for (auto cand : newCands) {
                     SMTsolver.resetSolver();
-                    SMTsolver.assertProp(logic.mkAnd(cand, logic.mkNot(logic.mkOr(strictCandidates))));
+                    SMTsolver.assertProp(logic.mkAnd(cand, logic.mkNot(oldInv)));
+                    // SMTsolver.assertProp(logic.mkAnd(cand, logic.mkNot(logic.mkOr(strictCandidates))));
                     if (SMTsolver.check() == SMTSolver::Answer::SAT) {
                         strictCandidates.push(cand);
                         change++;
@@ -835,7 +836,7 @@ ReachabilityNonterm::analyzeTS(PTRef init, PTRef transition, PTRef sink, Options
                 auto res = engine->solve(*graph);
                 if (res.getAnswer() == VerificationAnswer::SAFE) {
                     std::cout << "States are unreachable\n";
-                    std::cout << "TrInv: " << logic.pp(trInv) << std::endl;
+                    // std::cout << "TrInv: " << logic.pp(trInv) << std::endl;
                     return {Answer::YES, trInv};
                 } else {
                     // Otherwise, if states not covered by TrInv are reachable, then the following procedure should
