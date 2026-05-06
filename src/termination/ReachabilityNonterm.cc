1610a6c1508aad394c4078fb4d0280fdc995eae7
BritikovKI <BritikovKI@yandex.ru>
2026-05-06 14:29:30 +0200

    Model-based DNFization

--- src/termination/ReachabilityNonterm.cc
+++ src/termination/ReachabilityNonterm.cc
@@ -384,7 +384,7 @@ PTRef toDNF(PTRef formula, Logic &logic, int bound = 0) {
     PTRef phi = formula;
     SMTSolver smt_solver(logic, SMTSolver::WitnessProduction::ONLY_MODEL);
     smt_solver.assertProp(phi);
-    // std::cout << "Formula: "  << logic.pp(formula) << std::endl;
+    std::cout << "Pre DNF: "  << logic.pp(formula) << std::endl;
     while (smt_solver.check() == SMTSolver::Answer::SAT && (bound > 0 || unlimited)) {
         auto model = smt_solver.getModel();
         vec<PTRef> activeLiterals;
@@ -395,6 +395,7 @@ PTRef toDNF(PTRef formula, Logic &logic, int bound = 0) {
         smt_solver.push();
         smt_solver.assertProp(logic.mkNot(cube));
     }
+    std::cout << "Post DNF: "  << logic.pp(DNF) << std::endl;
     return DNF;
 }
 
