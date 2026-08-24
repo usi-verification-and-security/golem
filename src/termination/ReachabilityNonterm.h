a0ea4962a1e81236cd90eda850bebb4ed7a3d439
BritikovKI <BritikovKI@yandex.ru>
2026-08-24 19:05:45 +0200

    Moving code to functions, step 2

--- src/termination/ReachabilityNonterm.h
+++ src/termination/ReachabilityNonterm.h
@@ -32,11 +32,14 @@ private:
 
     std::tuple<Answer, PTRef> analyzeTS(PTRef init, PTRef transition, PTRef sink, ArithLogic & logic);
 
-    std::tuple<PTRef, PTRef> blockDeterministicPrefix(PTRef init, PTRef transition, PTRef sink, PTRef trace, uint num, ArithLogic & logic);
+    std::tuple<PTRef, PTRef> blockDeterministicPrefix(PTRef init, PTRef transition, PTRef sink, PTRef trace, uint num,
+                                                      ArithLogic & logic);
 
-    std::tuple<Answer, PTRef> tryTransitionInvariant(PTRef transition, PTRef sink, PTRef trace, uint num, ArithLogic & logic, vec<PTRef> & strictCandidates);
+    std::tuple<Answer, PTRef> tryTransitionInvariant(PTRef transition, PTRef sink, PTRef trace, uint num,
+                                                     ArithLogic & logic, vec<PTRef> & strictCandidates);
 
-    std::tuple<Answer, PTRef> refineTransitionInvariant(PTRef init, PTRef transition, PTRef originalTransition, PTRef sink, ArithLogic & logic, vec<PTRef> & strictCandidates);
+    std::tuple<Answer, PTRef> refineTransitionInvariant(PTRef init, PTRef transition, PTRef originalTransition,
+                                                        PTRef sink, ArithLogic & logic, vec<PTRef> & strictCandidates);
 };
 } // namespace golem::termination
 
