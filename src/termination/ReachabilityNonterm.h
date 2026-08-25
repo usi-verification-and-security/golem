4074c4c792337ba69b8d1202bf02ebbcfa84e8ef
BritikovKI <BritikovKI@yandex.ru>
2026-08-25 15:25:11 +0200

    Moving code to functions, step 2

--- src/termination/ReachabilityNonterm.h
+++ src/termination/ReachabilityNonterm.h
@@ -38,7 +38,7 @@ private:
     std::tuple<Answer, PTRef> tryTransitionInvariant(PTRef transition, PTRef sink, PTRef trace, uint num,
                                                      ArithLogic & logic, vec<PTRef> & strictCandidates);
 
-    std::tuple<Answer, PTRef> refineTransitionInvariant(PTRef init, PTRef transition, PTRef originalTransition,
+    std::tuple<Answer, PTRef> refineTransitionInvariant(PTRef init, PTRef transition,
                                                         PTRef & sink, ArithLogic & logic, vec<PTRef> & strictCandidates);
 };
 } // namespace golem::termination
