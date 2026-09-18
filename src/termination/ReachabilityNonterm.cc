606c8d265b0fbea01713cc852ee1bbdd36aaeb67
BritikovKI <BritikovKI@yandex.ru>
2026-09-18 16:08:29 +0200

    Original check

--- src/termination/ReachabilityNonterm.cc
+++ src/termination/ReachabilityNonterm.cc
@@ -715,7 +715,7 @@ std::tuple<ReachabilityNonterm::Answer, PTRef> ReachabilityNonterm::checkTermina
                      logic.mkNot(shiftOnlyNextVars(trInv, vars, logic))}),
         vars);
     // TODO: try adding covered to sink (new noncovered)
-    covered = TermUtils(logic).simplifyMax(logic.mkOr(covered, logic.mkNot(noncoveredStates)));
+    covered = TermUtils(logic).simplifyMax(logic.mkOr(covered, normalize(logic.mkNot(noncoveredStates), logic)));
 
     // We check if the states that are not covered by TrInv are reachable
     auto graph =
@@ -777,8 +777,12 @@ std::tuple<ReachabilityNonterm::Answer, PTRef> ReachabilityNonterm::checkTermina
         strictCandidates.clear();
         strictCandidates.push(subinv);
         strictCandidates.push(trInv);
+        PTRef newCov = QuantifierElimination(logic).keepOnly(
+            logic.mkAnd({logic.mkOr(subinv, id), TimeMachine(logic).sendFlaThroughTime(transition, 1),
+                         logic.mkNot(shiftOnlyNextVars(subinv, vars, logic))}),
+            vars);
         // TODO: Think if maybe sink can be even more restricted...
-        sink = logic.mkAnd(sink, reached);
+        sink = TermUtils(logic).simplifyMax(logic.mkOr(sink, newCov));
         smt_checker.resetSolver();
         // TODO: It should work for  subinv \/ TrInv, but it does not
         //    weaker TrInv seems to fail more often then stronger TrInv :(
